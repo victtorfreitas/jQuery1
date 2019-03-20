ae34a41623f440705c02476ae310f1fdfaffd617
Victtor A. Freitas <VicttorFreitas95@gmail.com>
2019-03-20 18:29:56 -0300

    Aplicando estilo e verificando texto

--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,75 @@
-contaPalavrasFrase();
-let campoTempo = $('#segundos');
-let contador = {};
-let intervalo;
+let tempo = $('#segundos');
+let tempoInicial = $('#segundos').text();
+let campoDigitacao = $('.campo-digitacao');
+let frase = $('#frase').text();
 
-
-
-
+$(function () {
+    contaPalavrasFrase();
+    inputValores();
+    iniciaCronometro();
+});
 
 function contaPalavrasFrase() {
-    $('#palavras').text(contacaoDePalavras('#frase'));
+    let palavrasContadas = frase.split(/\S+/).length;
+    $('#palavras').text(palavrasContadas - 1);
 }
 
 function contaPalavrasDigitadas() {
-    $('.qtd-palavras').text(contacaoDePalavrasInput('.campo-digitacao'));
+    let palavras = campoDigitacao.val().split(/\S+/).length;
+    $('.qtd-palavras').text(palavras - 1);
 }
-function contacaoDePalavras(id) {
-    let palavras = $(id).text().split(/\S+/);
-    return palavras.length - 1;
-}
-function contacaoDePalavrasInput(id) {
-    let palavras = $(id).val().split(/\S+/);
-    return palavras.length - 1;
-}
-
-contador.stop = function () {
-    clearInterval(intervalo);
-}
-function gameOver() {
-    contador.stop();
-    $('.campo-digitacao').attr('disabled', 'true');
+function contaCaracteresDigitados() {
+    $('.qtd-caracteres').text(campoDigitacao.val().length);
 }
 
 function contaSegundos() {
-    let tempo = campoTempo.text();
-    intervalo = setInterval(() => {
-        campoTempo.text(tempo--);
-        if (tempo < 0) {
-            gameOver();
+    let tempoRestante = tempo.text();
+    let cronometro = setInterval(() => {
+        tempo.text(tempoRestante--);
+        if (tempoRestante < 0) {
+            campoDigitacao.attr('disabled', true);
+            $('#btn-reset').attr('disabled', false);
+            campoDigitacao.toggleClass('campo-desativado');
+            clearInterval(cronometro);
         }
     }, 1000);
 }
 
+function inputValores() {
+    campoDigitacao.on('input', function (e) {
+        e.preventDefault();
+        contaCaracteresDigitados();
+        contaPalavrasDigitadas();
+        comparaValoresDigitados();
+    });
+}
 
-$('.campo-digitacao').on('input', function (e) {
-    e.preventDefault();
-    let campo = $('.campo-digitacao').val();
-    $('.qtd-caracteres').text(campo.length);
+function comparaValoresDigitados() {
+    if (frase.startsWith(campoDigitacao.val())) {
+        campoDigitacao.addClass('borda-certa');
+        campoDigitacao.removeClass('borda-errada');
+    } else {
+        campoDigitacao.addClass('borda-errada');
+        campoDigitacao.removeClass('borda-certa');
+    }
+}
+
+function iniciaCronometro() {
+    $('.campo-digitacao').one('focus', function (e) {
+        e.preventDefault();
+        $('#btn-reset').attr('disabled', true);
+        contaSegundos();
+    });
+}
+$('#btn-reset').click(function () {
+    campoDigitacao.attr('disabled', false);
+    campoDigitacao.val("");
+    campoDigitacao.toggleClass('campo-desativado');
     contaPalavrasDigitadas();
-});
-$('.campo-digitacao').one('focus',function (e) {
-    e.preventDefault();
-    contaSegundos();
+    contaCaracteresDigitados();
+    tempo.text(tempoInicial);
+    iniciaCronometro();
+    campoDigitacao.removeClass('borda-certa');
+    campoDigitacao.removeClass('borda-errada');
 });
 
